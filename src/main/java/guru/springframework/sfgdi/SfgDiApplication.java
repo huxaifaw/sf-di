a5f6563afd8f88523307b3e28d0b001db8e71bcf
HUZAIFA <huzaifawaseem97@gmail.com>
2022-06-20 02:58:55 +0500

    Assignment Solution

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -20,14 +20,14 @@ public class SfgDiApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		System.out.println("------- Primary Bean");
+		System.out.println("-------- Primary Bean");
 		System.out.println(myController.sayHello());
 
-		System.out.println("------ Property");
+		System.out.println("-------- Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 
-		System.out.println("--------- Setter");
+		System.out.println("-------- Setter");
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
 		System.out.println(setterInjectedController.getGreeting());
 
